05e8c30cd10f3f6f504719269c75f6b42bd1a2b6
fallOut015 <captainDoodles264@gmail.com>
2020-04-09 23:26:27 -0700

    Mo' changes

--- main/java/two/entity/EntityTypeTwo.java
+++ main/java/two/entity/EntityTypeTwo.java
@@ -7,14 +7,14 @@ import net.minecraft.entity.EntityType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.registries.ObjectHolder;
 import two.entity.monster.ShadowEntity;
-//import two.entity.monster.WandererEntity;
+import two.entity.monster.WandererEntity;
 import two.entity.passive.ChameleonEntity;
 
 @ObjectHolder("two")
 @SuppressWarnings("unchecked")
 public class EntityTypeTwo {
 	public static EntityType<ChameleonEntity> CHAMELEON = (EntityType<ChameleonEntity>) register("chameleon", EntityType.Builder.create(ChameleonEntity::new, EntityClassification.CREATURE).size(0.25f, 0.25f).build("chameleon"));
-	//public static EntityType<WandererEntity> WANDERER = (EntityType<WandererEntity>) register("wanderer", EntityType.Builder.create(WandererEntity::new, EntityClassification.MONSTER).size(0.5f, 1.0f).build("wanderer"));
+	public static EntityType<WandererEntity> WANDERER = (EntityType<WandererEntity>) register("wanderer", EntityType.Builder.create(WandererEntity::new, EntityClassification.MONSTER).size(0.5f, 1.0f).build("wanderer"));
 	public static EntityType<ShadowEntity> SHADOW = (EntityType<ShadowEntity>) register("shadow", EntityType.Builder.create(ShadowEntity::new, EntityClassification.MONSTER).size(0.5f, 1.0f).build("shadow"));
 	
 	public static void onEntityTypesRegistry(final RegistryEvent.Register<EntityType<?>> entityTypeRegistryEvent) {
