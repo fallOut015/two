121fae790d13ca10e981b4b55545eada6a49a364
fallOut015 <captainDoodles264@gmail.com>
2020-07-02 12:48:36 -0700

    closer to getting conveyer block working
    not gonna fret until 2.4 or whatever

--- main/java/two/block/ConveyerBlock.java
+++ main/java/two/block/ConveyerBlock.java
@@ -1,6 +1,13 @@
 package two.block;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.RotatedPillarBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.MoverType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 public class ConveyerBlock extends RotatedPillarBlock {
 	public ConveyerBlock(Properties properties) {
@@ -11,4 +18,22 @@ public class ConveyerBlock extends RotatedPillarBlock {
 //	public void func_225534_a_(BlockState blockState, ServerWorld serverWorld, BlockPos blockPos, Random random) {
 		// TODO push entities
 //	}
+	
+	@Override
+	public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+		if(!(entityIn.getPosY() > pos.getY())) {
+			return;
+		}
+		Vec3d destination = entityIn.getPositionVec();
+		if(state.get(AXIS).test(Direction.NORTH)) {
+			destination.add(1, 0, 0);
+		} else if(state.get(AXIS).test(Direction.EAST)) {
+			destination.add(1, 0, 0);
+		} else if(state.get(AXIS).test(Direction.SOUTH)) {
+			destination.add(-1, 0, 0);
+		} else if(state.get(AXIS).test(Direction.WEST)) {
+			destination.add(-1, 0, 0);
+		}
+		entityIn.move(MoverType.SELF, destination);
+	}
 }
