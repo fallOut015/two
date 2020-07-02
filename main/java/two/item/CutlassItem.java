164c556a44644a02121d72d3488192f754139ec2
fallOut015 <captainDoodles264@gmail.com>
2020-07-01 21:17:59 -0700

    removed sound debugging code

--- main/java/two/item/CutlassItem.java
+++ main/java/two/item/CutlassItem.java
@@ -6,9 +6,7 @@ import net.minecraft.item.ItemTier;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
-import two.util.SoundEventsTwo;
 
 public class CutlassItem extends SwordItem {
 	public CutlassItem(Properties builder) {
@@ -16,9 +14,6 @@ public class CutlassItem extends SwordItem {
 	}
 	
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-		// sound tests
-		worldIn.playSound(playerIn, playerIn.getPosition(), SoundEventsTwo.MUSIC_NIGHTMARE, SoundCategory.MUSIC, 100.0f, 1.0f);
-		worldIn.playSound(playerIn, playerIn.getPosition(), SoundEventsTwo.ITEM_ARMOR_EQUIP_EMERALD, SoundCategory.NEUTRAL, 100.0f, 1.0f);
 		// add code for sword blocking
 		return ActionResult.resultSuccess(playerIn.getActiveItemStack());
 	};
