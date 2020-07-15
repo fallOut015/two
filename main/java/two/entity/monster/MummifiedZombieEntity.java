f1ad09f8269aaa28c88f9e84ecc5c6aa83039817
fallOut015 <captainDoodles264@gmail.com>
2020-07-14 22:10:43 -0700

    Update MummifiedZombieEntity.java

--- main/java/two/entity/monster/MummifiedZombieEntity.java
+++ main/java/two/entity/monster/MummifiedZombieEntity.java
@@ -1,11 +1,23 @@
 package two.entity.monster;
 
+import java.util.Random;
+
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
 public class MummifiedZombieEntity extends ZombieEntity {
 	public MummifiedZombieEntity(EntityType<? extends ZombieEntity> type, World worldIn) {
 		super(type, worldIn);
+	}
+	
+	public static boolean canSpawn(EntityType<MummifiedZombieEntity> batIn, IWorld worldIn, SpawnReason reason, BlockPos pos, Random randomIn) {
+		if (pos.getY() >= worldIn.getSeaLevel()) {
+			return false;
+		}
+		return true;
 	}
 }
