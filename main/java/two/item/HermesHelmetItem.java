c3d8b1b288196789a3f82ffc21603efdd20e4d95
fallOut015 <captainDoodles264@gmail.com>
2020-07-24 11:44:55 -0700

    Update HermesHelmetItem.java
    does stuff
    
    what kind of stuff
    
    we're not doing this again

--- main/java/two/item/HermesHelmetItem.java
+++ main/java/two/item/HermesHelmetItem.java
@@ -1,10 +1,29 @@
 package two.item;
 
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.world.World;
+import two.enchantment.EnchantmentsTwo;
 
 public class HermesHelmetItem extends ArmorItem {
 	public HermesHelmetItem(Properties builder) {
 		super(ArmorMaterialTwo.HERMES_HELMET, EquipmentSlotType.HEAD, builder);
+	}
+	
+	@Override
+	public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+		int amplifierIn = EnchantmentHelper.getEnchantmentLevel(EnchantmentsTwo.GENTLE_DESCENT, stack);
+		
+		if(player.getActivePotionEffect(Effects.SLOW_FALLING) == null) {
+			player.addPotionEffect(new EffectInstance(Effects.SLOW_FALLING, 1, amplifierIn, true, false));
+		} else {
+			player.getActivePotionEffect(Effects.SLOW_FALLING).combine(new EffectInstance(Effects.SLOW_FALLING, 1, amplifierIn, true, false));
+		}
+		super.onArmorTick(stack, world, player);
 	}
 }
