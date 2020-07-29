07a62538ce06faa89a51fa7657a722734f0a0609
fallOut015 <captainDoodles264@gmail.com>
2020-07-28 17:34:18 -0700

    fixed and finished some bearded dragon stuff

--- main/java/two/client/renderer/entity/model/BeardedDragonModel.java
+++ main/java/two/client/renderer/entity/model/BeardedDragonModel.java
@@ -17,7 +17,6 @@ public class BeardedDragonModel<T extends BeardedDragonEntity> extends EntityMod
     public ModelRenderer bodyMiddle;
     public ModelRenderer tailFront;
     public ModelRenderer tongueA;
-    public ModelRenderer tongueB;
     public ModelRenderer bodyTop;
     public ModelRenderer legTopRight;
     public ModelRenderer legTopLeft;
@@ -36,7 +35,7 @@ public class BeardedDragonModel<T extends BeardedDragonEntity> extends EntityMod
         this.textureHeight = 32;
         this.tongueA = new ModelRenderer(this, 0, 0);
         this.tongueA.setRotationPoint(0.0F, 0.0F, -2.0F);
-        this.tongueA.addBox(-0.5F, 0.0F, -1.0F, 1, 0, 2, 0.0F);
+        this.tongueA.addBox(-0.5F, 0.0F, -1.0F, 1, 0.1f, 2, 0.0F);
         this.tailFront = new ModelRenderer(this, 16, 0);
         this.tailFront.setRotationPoint(0.0F, 23.5F, 2.9F);
         this.tailFront.addBox(-1.0F, -0.5F, 0.0F, 2, 1, 4, 0.0F);
@@ -47,19 +46,15 @@ public class BeardedDragonModel<T extends BeardedDragonEntity> extends EntityMod
         this.setRotateAngle(legTopRight, 0.0F, -0.7853981633974483F, 0.0F);
         this.legBottomRightFoot = new ModelRenderer(this, 0, 0);
         this.legBottomRightFoot.setRotationPoint(1.5F, 0.3F, 0.0F);
-        this.legBottomRightFoot.addBox(-0.5F, 0.0F, -0.5F, 1, 0, 1, 0.0F);
+        this.legBottomRightFoot.addBox(-0.5F, 0.0F, -0.5F, 1, 0.1f, 1, 0.0F);
         this.tailMiddleA = new ModelRenderer(this, 54, 0);
         this.tailMiddleA.mirror = true;
         this.tailMiddleA.setRotationPoint(0.0F, -0.2F, 4.8F);
         this.tailMiddleA.addBox(-0.5F, -0.5F, -1.0F, 1, 1, 2, 0.0F);
         this.setRotateAngle(tailMiddleA, 0.17453292519943295F, 0.0F, 0.0F);
-        this.tongueB = new ModelRenderer(this, 0, 0);
-        this.tongueB.setRotationPoint(0.0F, 0.0F, -2.0F);
-        this.tongueB.addBox(-0.5F, 0.0F, -1.0F, 1, 0, 2, 0.0F);
-        this.setRotateAngle(tongueB, 3.141592653589793F, 0.0F, 0.0F);
         this.legBottomLeftFoot = new ModelRenderer(this, 0, 0);
         this.legBottomLeftFoot.setRotationPoint(1.5F, 0.3F, 0.0F);
-        this.legBottomLeftFoot.addBox(-0.5F, 0.0F, -0.5F, 1, 0, 1, 0.0F);
+        this.legBottomLeftFoot.addBox(-0.5F, 0.0F, -0.5F, 1, 0.1f, 1, 0.0F);
         this.tailMiddleB = new ModelRenderer(this, 16, 0);
         this.tailMiddleB.mirror = true;
         this.tailMiddleB.setRotationPoint(0.0F, -0.2F, 1.3F);
@@ -67,10 +62,10 @@ public class BeardedDragonModel<T extends BeardedDragonEntity> extends EntityMod
         this.setRotateAngle(tailMiddleB, 0.3490658503988659F, 0.0F, 0.0F);
         this.legTopRightFoot = new ModelRenderer(this, 0, 0);
         this.legTopRightFoot.setRotationPoint(-1.5F, 0.3F, 0.0F);
-        this.legTopRightFoot.addBox(-0.5F, 0.0F, -0.5F, 1, 0, 1, 0.0F);
+        this.legTopRightFoot.addBox(-0.5F, 0.0F, -0.5F, 1, 0.1f, 1, 0.0F);
         this.legTopLeftFoot = new ModelRenderer(this, 0, 0);
         this.legTopLeftFoot.setRotationPoint(-1.5F, 0.3F, 0.0F);
-        this.legTopLeftFoot.addBox(-0.5F, 0.0F, -0.5F, 1, 0, 1, 0.0F);
+        this.legTopLeftFoot.addBox(-0.5F, 0.0F, -0.5F, 1, 0.1f, 1, 0.0F);
         this.legTopLeft = new ModelRenderer(this, 38, 0);
         this.legTopLeft.setRotationPoint(-2.3F, 0.5F, 2.5F);
         this.legTopLeft.addBox(-1.0F, -0.5F, -0.5F, 2, 1, 1, 0.0F);
@@ -101,7 +96,6 @@ public class BeardedDragonModel<T extends BeardedDragonEntity> extends EntityMod
         this.bodyMiddle.addChild(this.legTopRight);
         this.legBottomRight.addChild(this.legBottomRightFoot);
         this.tailFront.addChild(this.tailMiddleA);
-        this.head.addChild(this.tongueB);
         this.legBottomLeft.addChild(this.legBottomLeftFoot);
         this.tailMiddleA.addChild(this.tailMiddleB);
         this.legTopRight.addChild(this.legTopRightFoot);
@@ -121,16 +115,22 @@ public class BeardedDragonModel<T extends BeardedDragonEntity> extends EntityMod
     
 	@Override
 	public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-		this.legBottomLeft.rotateAngleY = MathHelper.cos(limbSwing * 0.06662F + (float)Math.PI) * -1.8F * limbSwingAmount - 0.5F;
-    	//this.LegTopRight.rotateAngleY = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * -1.8F * limbSwingAmount - 0.5F;
-    	this.legBottomRight.rotateAngleY = MathHelper.cos(limbSwing * 0.06662F + (float)Math.PI) * 1.8F * limbSwingAmount + 0.5F;
-    	//this.LegTopLeft.rotateAngleY = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.8F * limbSwingAmount + 0.5F;
+		this.legBottomLeft.rotateAngleY = MathHelper.cos(limbSwing * 3.6662F) * 1.4F * limbSwingAmount - 0.7853981633974483F;
+		this.legTopRight.rotateAngleY = MathHelper.cos(limbSwing * 3.6662F + (float)Math.PI) * 1.4F * limbSwingAmount - 0.7853981633974483F;
+		this.legBottomRight.rotateAngleY = MathHelper.cos(limbSwing * 3.6662F) * 1.4F * limbSwingAmount + 0.7853981633974483F;
+		this.legTopLeft.rotateAngleY = MathHelper.cos(limbSwing * 3.6662F + (float)Math.PI) * 1.4F * limbSwingAmount + 0.7853981633974483F;
     	this.tailFront.rotateAngleY = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 0.9F * limbSwingAmount; 
 	}
 	@Override
 	public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
 		ImmutableList.of(this.head, this.bodyMiddle, this.tailFront).forEach(modelRenderer -> {
 			modelRenderer.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
+		});
+	}
+	
+	public void renderOnShoulder(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float p_228284_5_, float p_228284_6_, float p_228284_7_, float p_228284_8_, int p_228284_9_) {
+		ImmutableList.of(this.head, this.bodyMiddle, this.tailFront).forEach(modelRenderer -> {
+			modelRenderer.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, 1.0f, 1.0f, 1.0f, 1.0f);
 		});
 	}
 }
--- main/java/two/entity/passive/BeardedDragonEntity.java
+++ main/java/two/entity/passive/BeardedDragonEntity.java
@@ -7,8 +7,10 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.goal.BreedGoal;
 import net.minecraft.entity.ai.goal.FollowOwnerGoal;
+import net.minecraft.entity.ai.goal.LandOnOwnersShoulderGoal;
 import net.minecraft.entity.ai.goal.LookAtGoal;
 import net.minecraft.entity.ai.goal.PanicGoal;
+import net.minecraft.entity.ai.goal.TemptGoal;
 import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
 import net.minecraft.entity.passive.ShoulderRidingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,11 +18,14 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import two.entity.EntityTypeTwo;
 
 public class BeardedDragonEntity extends ShoulderRidingEntity {
+	private static final Ingredient TEMPTATION_ITEMS = Ingredient.fromItems(Items.SPIDER_EYE);
+
 	public BeardedDragonEntity(EntityType<? extends BeardedDragonEntity> type, World worldIn) {
 		super(type, worldIn);
 		this.setTamed(false);
@@ -38,17 +43,23 @@ public class BeardedDragonEntity extends ShoulderRidingEntity {
 	}
 	@Override
 	protected void registerGoals() {
-	    this.goalSelector.addGoal(6, new FollowOwnerGoal(this, 2.0D, 1.0F, 9.0F, false));
-		this.goalSelector.addGoal(2, new BreedGoal(this, 0.8D));
-		this.goalSelector.addGoal(3, new WaterAvoidingRandomWalkingGoal(this, 0.8D, 1.0000001E-5F));
-		this.goalSelector.addGoal(4, new LookAtGoal(this, PlayerEntity.class, 10.0F));
-		this.goalSelector.addGoal(5, new PanicGoal(this, 2.0D));
+	    this.goalSelector.addGoal(1, new FollowOwnerGoal(this, 1.0D, 1.0F, 9.0F, false));
+		this.goalSelector.addGoal(2, new LandOnOwnersShoulderGoal(this));
+		this.goalSelector.addGoal(3, new PanicGoal(this, 2.0D));
+		this.goalSelector.addGoal(4, new WaterAvoidingRandomWalkingGoal(this, 0.8D, 1.0000001E-5F));
+		this.goalSelector.addGoal(5, new LookAtGoal(this, PlayerEntity.class, 10.0F));
+		this.goalSelector.addGoal(6, new BreedGoal(this, 0.8D));
+		this.goalSelector.addGoal(7, new TemptGoal(this, 1.2D, false, TEMPTATION_ITEMS));
 	}
 	@Override
 	protected void registerAttributes() {
 		super.registerAttributes();
 		
-		this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(4);
+		if (this.isTamed()) {
+			this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(8.0D);
+		} else {
+			this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(4.0D);
+		}	
 		this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3);
 	}
 	@Override
@@ -96,5 +107,15 @@ public class BeardedDragonEntity extends ShoulderRidingEntity {
 	public boolean isBreedingItem(ItemStack stack) {
 		Item item = stack.getItem();
 		return item == Items.SPIDER_EYE;
+	}
+	@Override
+	public void setTamed(boolean tamed) {
+		super.setTamed(tamed);
+		if (tamed) {
+			this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10.0D);
+			this.setHealth(10.0F);
+		} else {
+			this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(5.0D);
+		}
 	}
 }
