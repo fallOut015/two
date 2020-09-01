7d9c96a35db6b4d54a18cd78b05c3709f13fdbe0
fallOut015 <captainDoodles264@gmail.com>
2020-08-31 22:36:12 -0700

    fixed helmet testing

--- main/java/io/github/fallout015/two/client/renderer/entity/layers/CrownLayer.java
+++ main/java/io/github/fallout015/two/client/renderer/entity/layers/CrownLayer.java
@@ -1,8 +1,5 @@
 package io.github.fallout015.two.client.renderer.entity.layers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 
@@ -15,7 +12,6 @@ import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.PlayerModel;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ArmorItem;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -34,12 +30,7 @@ public class CrownLayer extends LayerRenderer<AbstractClientPlayerEntity, Player
 	@Override
 	public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, AbstractClientPlayerEntity entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
 	    if("Dev".equals(entitylivingbaseIn.getName().getString()) && entitylivingbaseIn.hasSkin() && !entitylivingbaseIn.isInvisible()) {
-	    	List<ArmorItem> armor = new ArrayList<ArmorItem>();
-	    	entitylivingbaseIn.getArmorInventoryList().forEach(itemStack -> {
-	    		if(itemStack.getItem() instanceof ArmorItem)
-	    			armor.add((ArmorItem) itemStack.getItem());
-	    	});
-	    	if(!armor.removeIf(item -> item.getEquipmentSlot() == EquipmentSlotType.HEAD)) {
+	    	if(entitylivingbaseIn.getItemStackFromSlot(EquipmentSlotType.HEAD).isEmpty()) {
 	    		IVertexBuilder ivertexbuilder = bufferIn.getBuffer(RenderType.getEntitySolid(TEXTURE));
 		    	int i = LivingRenderer.getPackedOverlay(entitylivingbaseIn, 0.0F);
 
