2c6b195b5e454bf6da270004d32375b40bb69232
fallOut015 <captainDoodles264@gmail.com>
2020-09-02 15:31:39 -0700

    sigil is no longer a living entity

--- main/java/io/github/fallout015/two/client/renderer/entity/SigilRenderer.java
+++ main/java/io/github/fallout015/two/client/renderer/entity/SigilRenderer.java
@@ -1,15 +1,14 @@
 package io.github.fallout015.two.client.renderer.entity;
 
-import io.github.fallout015.two.client.renderer.entity.model.SigilModel;
 import io.github.fallout015.two.entity.effect.SigilEntity;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class SigilRenderer extends LivingRenderer<SigilEntity, SigilModel<SigilEntity>> {
+public class SigilRenderer extends EntityRenderer<SigilEntity> {
 	public static final ResourceLocation[] TEXTURE = {
 		new ResourceLocation("two", "textures/entity/sigil/sigil-1.png"),
 		new ResourceLocation("two", "textures/entity/sigil/sigil-2.png"),
@@ -18,15 +17,11 @@ public class SigilRenderer extends LivingRenderer<SigilEntity, SigilModel<SigilE
 	};
 	
 	public SigilRenderer(EntityRendererManager rendererManager) {
-		super(rendererManager, new SigilModel<>(), 0);
+		super(rendererManager);
 	}
 
 	@Override
 	public ResourceLocation getEntityTexture(SigilEntity entity) {
 		return TEXTURE[(entity.ticksExisted % 16) / 4];
-	}
-	@Override
-	protected boolean canRenderName(SigilEntity entity) {
-		return false;
 	}
 }
--- main/java/io/github/fallout015/two/entity/effect/SigilEntity.java
+++ main/java/io/github/fallout015/two/entity/effect/SigilEntity.java
@@ -1,39 +1,20 @@
 package io.github.fallout015.two.entity.effect;
 
 import io.github.fallout015.two.entity.EntityTypeTwo;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.IPacket;
 import net.minecraft.particles.ParticleTypes;
-import net.minecraft.util.HandSide;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
-public class SigilEntity extends LivingEntity { // TODO change to normal entity
-	public SigilEntity(EntityType<? extends LivingEntity> type, World worldIn) {
-		super(EntityTypeTwo.SIGIL, worldIn);
+public class SigilEntity extends Entity {
+	public SigilEntity(EntityType<? extends Entity> type, World worldIn) {
+		super(EntityTypeTwo.SIGIL, worldIn); // super(type)? TODO
 	}
 
-	@Override
-	public Iterable<ItemStack> getArmorInventoryList() {
-		return NonNullList.from(ItemStack.EMPTY);
-	}
-	@Override
-	public ItemStack getItemStackFromSlot(EquipmentSlotType slotIn) {
-		return ItemStack.EMPTY;
-	}
-	@Override
-	public void setItemStackToSlot(EquipmentSlotType slotIn, ItemStack stack) { }
-	@Override
-	public HandSide getPrimaryHand() {
-		return HandSide.LEFT;
-	}
-	@Override
-	public boolean hasNoGravity() {
-		return true;
-	}
 	@Override
 	public void tick() {
 		this.noClip = true;
@@ -45,20 +26,19 @@ public class SigilEntity extends LivingEntity { // TODO change to normal entity
 		this.getEntityWorld().addParticle(ParticleTypes.FIREWORK, x, y, z, 0, this.rand.nextInt() % 2, 0);
 	}
 	@Override
-	public boolean canBePushed() {
-		return false;
-	}
-	@Override
-	public boolean canAttack(EntityType<?> typeIn) {
-		return false;
-	}
-	@Override
-	public boolean canBeAttackedWithItem() {
-		return false;
-	}
-	
-	@Override
 	protected void registerData() {
-		super.registerData();
+		// TODO Auto-generated method stub
+	}
+	@Override
+	protected void readAdditional(CompoundNBT compound) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	protected void writeAdditional(CompoundNBT compound) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public IPacket<?> createSpawnPacket() {
+		return NetworkHooks.getEntitySpawningPacket(this);
 	}
 }
