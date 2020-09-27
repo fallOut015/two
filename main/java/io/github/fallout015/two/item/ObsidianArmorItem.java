b3acaa1cbbb8393ebabf8a1b412b91ed696d8b2e
fallOut015 <captainDoodles264@gmail.com>
2020-09-27 11:30:20 -0700

    created item classes for glowstone and obsidian armors

---
+++ main/java/io/github/fallout015/two/item/ObsidianArmorItem.java
@@ -0,0 +1,19 @@
+package io.github.fallout015.two.item;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class ObsidianArmorItem extends ArmorItem {
+	public ObsidianArmorItem(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
+		super(materialIn, slot, builderIn);
+	}
+
+	@Override
+	public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+		player.addTag("fireproof");
+	}
+}
